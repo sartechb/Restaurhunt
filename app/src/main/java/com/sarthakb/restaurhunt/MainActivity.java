b18199ccb0cf33b0825a05348fdc01ef4c511e9d
sartechb <sarthak1657@gmail.com>
2016-10-08 01:41:24 -0400

    more init stuff

--- app/src/main/java/com/sarthakb/restaurhunt/MainActivity.java
+++ app/src/main/java/com/sarthakb/restaurhunt/MainActivity.java
@@ -2,12 +2,50 @@ package com.sarthakb.restaurhunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_main);
+        toolbar.setTitle("Restaurhunt");
+        setSupportActionBar(toolbar);
+
+        AccountHeader profileHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .addProfiles(
+                        new ProfileDrawerItem()
+                        .withName("Test McTestFace")
+                        .withEmail("hello@sarthakb.com")
+                ).build();
+
+        PrimaryDrawerItem imagesItem = new PrimaryDrawerItem()
+                .withIdentifier(1).withName("Images");
+        PrimaryDrawerItem uploadItem = new PrimaryDrawerItem().withIdentifier(2).withName("Upload");
+        PrimaryDrawerItem myImagesItem = new PrimaryDrawerItem().withIdentifier(2).withName("My Images");
+
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .withAccountHeader(profileHeader)
+                .addDrawerItems(imagesItem, uploadItem, myImagesItem)
+                .build();
     }
 }
