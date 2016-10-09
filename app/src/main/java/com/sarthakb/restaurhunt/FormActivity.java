a5f228ba123fd147ce8fb4e799be175fdee0b997
sartechb <sarthak1657@gmail.com>
2016-10-09 01:54:28 -0400

    form

---
+++ app/src/main/java/com/sarthakb/restaurhunt/FormActivity.java
@@ -0,0 +1,14 @@
+package com.sarthakb.restaurhunt;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class FormActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+
+}
